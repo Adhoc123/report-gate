7703eccd3059d91380326ef8be61e36929c22341
Adhoc123 <emonhasan1010@gmail.com>
2022-08-23 03:04:38 +0600

    added disable button

--- js/interact.js
+++ js/interact.js
@@ -1,13 +1,24 @@
 const playerNameArray = [];
 function display(playerList){
     const player = document.getElementById('player-list');
-   for(let i=0;i<playerList.length;i++){
-    console.log(playerList[i]);
+    player.innerHTML = "";
+    for(let i=0;i<playerList.length;i++){
+        if(i==5){
+            alert("You can't add more than five players");
+            return;
+        }
+    const name = playerList[i];
+    const li = document.createElement('li');
+    li.innerHTML = name;
+    player.appendChild(li);
    }
 
 }
 function addNameToList(feature){
     playerName = feature.parentNode.parentNode.children[0].innerText;
     playerNameArray.push(playerName);
+    if(playerNameArray.length<=5){
+         feature.disabled = true;
+    }
     display(playerNameArray);
 }
