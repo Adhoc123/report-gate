96ecec9303ddfd32a456d54a19dd2eac1487c2a6
Adhoc123 <emonhasan1010@gmail.com>
2022-08-23 02:40:58 +0600

    player name added

--- js/interact.js
+++ js/interact.js
@@ -1,1 +1,13 @@
-console.log('hello shf');
+const playerNameArray = [];
+function display(playerList){
+    const player = document.getElementById('player-list');
+   for(let i=0;i<playerList.length;i++){
+    console.log(playerList[i]);
+   }
+
+}
+function addNameToList(feature){
+    playerName = feature.parentNode.parentNode.children[0].innerText;
+    playerNameArray.push(playerName);
+    display(playerNameArray);
+}
