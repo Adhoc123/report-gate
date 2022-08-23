7e64d46a5b797301367f18bdc86ef011cbc13f76
Adhoc123 <emonhasan1010@gmail.com>
2022-08-23 14:51:12 +0600

    added validation

--- js/expense.js
+++ js/expense.js
@@ -1,9 +1,35 @@
-document.getElementById('calculate').addEventListener('click',function(){
-    const perPlayer = document.getElementById('per-player');
-    const perPlayerCostString = perPlayer.value;
-    const perPlayerCost = parseInt(perPlayerCostString);
+let perPlayerCost;
+function getInputFieldValue(inputId){
+    const field = document.getElementById(inputId);
+    const fieldString = field.value;
+    const fieldCost = parseInt(fieldString);
+    field.value = "";
+    return fieldCost;
+}
 
+function getPlayerExpense(){
+    const playerExpense = perPlayerCost*playerNameArray.length;
+    return playerExpense;
+}
+
+document.getElementById('calculate').addEventListener('click',function(){ 
+    perPlayerCost = getInputFieldValue('per-player');
+    if(isNaN(perPlayerCost)){
+        alert('Provide interger values to calculate');
+        return;
+    }
     const playerExpense = document.getElementById('player-expense');
-    playerExpense.innerText = perPlayerCost*playerNameArray.length;
-    perPlayer.value = "";
+    playerExpense.innerText = getPlayerExpense();
+})
+
+document.getElementById('calculate-total').addEventListener('click',function(){
+    
+    const managerCost = getInputFieldValue('manager');
+    const coachCost = getInputFieldValue('coach');
+    if(isNaN(managerCost && coachCost && getPlayerExpense())){
+        alert('Provide values to calculate');
+        return;
+    }
+    const total = document.getElementById('total');
+    total.innerText = managerCost + coachCost + getPlayerExpense();
 })
